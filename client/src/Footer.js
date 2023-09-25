e37a0e7723deae067f04b7458f951de14b385fd9
DeepakChaudhary <aspirantsd@gmail.com>
2023-09-26 02:38:11 +0530

    first commit

---
+++ client/src/Footer.js
@@ -0,0 +1,35 @@
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+    <footer class="footer mt-5" id="footer">
+        <div className='social'>
+            <p>Connect me through</p> 
+            <div className='icons'>
+            <a href='https://www.linkedin.com/in/deepakchaudhary47/'>
+            <img width="30" height="30" src="https://img.icons8.com/fluency/48/linkedin.png" alt="linkedin"/>
+            </a>
+
+            <a href='https://github.com/DeepakChaudhary47'>
+            <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/github.png" alt="github"/>
+            </a>
+            
+            <a href='https://www.instagram.com/deepakrt47/'>
+            <img width="30" height="30" src="https://img.icons8.com/color/48/instagram-new--v1.png" alt="instagram-new--v1"/>
+            </a>
+
+            <a href='https://join.skype.com/invite/wIaG7NNZKj6A'>
+            <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/skype.png" alt="skype"/>
+            </a>
+
+            </div>
+        </div>
+        <p class="f1">Designed By Deepak Chaudhary with🐼🖤</p>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
